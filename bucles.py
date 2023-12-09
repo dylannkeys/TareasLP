e499d483f402e9c41829560981bee29da5da4431
dylannkeys <dm096526@gmail.com>
2023-12-09 12:26:05 -0500

    bucles

---
+++ bucles.py
@@ -0,0 +1,40 @@
+import random
+import string
+import sys
+import subprocess
+
+def ns(r):
+    while r!="n" and r!="s":
+        r=input("Escriba solo \'n\' o \'s\' según su opción: ")
+    return r
+
+def OKI(n):
+    try:
+        n=int(n)
+    except:
+        n=OKI(input("Caracter no valido: "))
+    return n
+
+def opt(o,l):
+    while o not in l:
+        o=input("Escriba solo una de las opciónes posibles: ")
+    return o
+
+ops=sys.platform
+
+while True:
+    print("*******GENERADOR DE CONTRASEÑAS*******")
+    minus=OKI(input("Indique número mínimo de minusculas: "))
+    mayus=OKI(input("Indique número mínimo de mayusculas: "))
+    numeros=OKI(input("Indique número mínimo de caracteres numéricos: "))
+    longitud=OKI(input("Indique longitud de la contraseña: "))
+    suma=minus+mayus+numeros #SUMA DE MINIMOS
+    while longitud<suma: #COMPROBACION ADECUACIÓN DE LA "longitud".
+        longitud=OKI(input("Longitud inadecuada: "))
+    caract=string.ascii_letters+string.digits
+    while True:
+        contra=("").join(random.choice(caract)for i in range(longitud))
+        if(sum(c.islower() for c in contra)>=minus
+            and sum(c.isupper() for c in contra)>=mayus
+            and sum(c.isdigit() for c in contra)>=numeros):
+            break
